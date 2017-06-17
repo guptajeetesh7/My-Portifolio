906d8d3ac5f1b1818a68d90ee52417b2ad905e6c
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-06-17 20:30:19 +0530

    skills added

--- js/jquery.js
+++ js/jquery.js
@@ -3,17 +3,55 @@ $(document).ready(function(){
 	$("#animate-nav").fadeIn(2000);
 	$("#animate-home").fadeIn(2000);
 
-var val = 0;
+var html =0; var css = 0;
 
-var interval = setInterval(function(){
 
-	val = val + 1;
-	$("#html").progressbar({value : 50});
-	  $("#html > div").css({ 'background': '#2196F3' });;
+var interval_html = setInterval(function(){
 
-},50);
 
-  $("#html").css({ 'background-color': 'white' });
+	html = html + 1;
+
+	$("#html")
+    .progressbar({ value: html })
+    .children('.ui-progressbar-value')
+    .html(html + '%')
+    .css("display", "block")
+    .css({ 'background-color': '#2196F3' });
+
+    $("#html").css({ 'background-color': 'white' });
+
+
+	if(html==85){
+		clearInterval(interval_html);
+	}
+	  
+
+},20);
+
+
+
+var interval_css = setInterval(function(){
+
+
+	css = css + 1;
+
+	$("#css")
+    .progressbar({ value: html })
+    .children('.ui-progressbar-value')
+    .html(css + '%')
+    .css("display", "block")
+    .css({ 'background-color': '#673AB7' });
+
+    $("#html").css({ 'background-color': 'white' });
+
+
+	if(css==65){
+		clearInterval(interval_css);
+	}
+	  
+
+},20);
+  
   
 
 	
